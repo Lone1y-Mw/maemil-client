1d70f2deb985b02eef13ea3fe7a7cd7cf68af46d
minmoong <kim774634@gmail.com>
2022-05-06 15:27:46 +0900

    민뭉 - 기본적인 셋업

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,11 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import path from 'path';
+import alias from '@rollup/plugin-alias';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -29,7 +33,7 @@ function serve() {
 }
 
 export default {
-	input: 'src/main.js',
+	input: 'src/main.ts',
 	output: {
 		sourcemap: true,
 		format: 'iife',
@@ -38,6 +42,7 @@ export default {
 	},
 	plugins: [
 		svelte({
+			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
@@ -57,6 +62,18 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
+		typescript({
+			sourceMap: !production,
+			inlineSources: !production
+		}),
+		alias({
+			entries: [
+				{
+					find: "~",
+					replacement: path.resolve(__dirname, "src/")
+				}
+			]
+		}),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
---
+++ src/components/Footer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<div class="footer">
+    <div class="policy">개인정보처리방침</div>
+    <div class="copyright">© 2022 NSMS Programmers. All rights reserved.</div>
+</div>
+
+<style>
+    .footer {
+        text-align: center;
+        color: gray;
+    }
+
+    .policy {
+        text-decoration: underline;
+    }
+</style>
